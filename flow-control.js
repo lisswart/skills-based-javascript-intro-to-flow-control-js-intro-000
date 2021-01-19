4dd93521cf787c7a42dda2f0cb6015688587c1bb
Lisa Swart <arnoudlisa@gmail.com>
2021-01-19 04:03:02 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,17 +1,29 @@
 function basicTeenager(age) {
-
+  if (13 <= age <= 19) {
+    return `"You are a teenager!"`
+  }
 }
 
 function teenager(age) {
-
+  if (13 <= age <= 19) {
+    return `"You are a teenager!"`
+  } else {
+    return `"You are not a teenager!"`
+  }
 }
 
 function ageChecker(age) {
-
+  if (13 <= age <= 19) {
+    return `"You are a teenager!"`
+  } elseif (age <= 12) {
+    return `"You are a kid!"`
+  } else {
+    return `"You are a grownup"`
+  }
 }
 
 function ternaryTeenager(age) {
-
+  return 13 <= age <= 19? `"You are a teenager!"`:`"You are not a teenager!"`
 }
 
 function switchAge(age) {
